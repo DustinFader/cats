3b4a8cdc90da3df3c3e573d1b3dd528772bca1fe
DustinFader <dustinfader16@gmail.com>
2023-10-14 12:50:07 -0300

    Will now use mocha and fixed errors

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -6,15 +6,17 @@ const breedDetailsFromFile = function(breed, callback) {
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
 
     console.log("In readFile's Callback: it has the data.");
+    
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) callback(data);
+    
+    if (!error) {
+      callback(data);
+    } else callback(undefined)
   });
-  // ISSUE: Attempting to return data out here will also not work.
-  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
-// we try to get the return value
 const bombay = breedDetailsFromFile('Bombay', (data) => { console.log(data) });
+// we try to get the return value
 //console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
 
-// 34
+module.exports = breedDetailsFromFile;
